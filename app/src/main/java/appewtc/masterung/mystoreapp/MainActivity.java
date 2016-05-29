0602129563c83a1a87b704252ef1fdce2fe0b6ae
masterUNG <phrombutr@gmail.com>
2016-05-29 12:39:46 +0700

    Synchronize mySQL to SQLite finish

--- app/src/main/java/appewtc/masterung/mystoreapp/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mystoreapp/MainActivity.java
@@ -2,14 +2,24 @@ package appewtc.masterung.mystoreapp;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private MyManage myManage;
+    private static final String urlJSON = "http://swiftcodingthai.com/Moo/get_user_master.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +32,63 @@ public class MainActivity extends AppCompatActivity {
 
         deleteAllSQLite();
 
+        synMySQLtoSQLite();
+
     }   // Main Method
+
+    //Create Inner Class
+    public class ConnectedUserJSON extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String strJSON) {
+            super.onPostExecute(strJSON);
+
+            Log.d("Store", "strJSON ==> " + strJSON);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(strJSON);
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String strName = jsonObject.getString(MyManage.column_name);
+                    String strSurname = jsonObject.getString(MyManage.column_surname);
+                    String strUser = jsonObject.getString(MyManage.column_user);
+                    String strPassword = jsonObject.getString(MyManage.column_password);
+                    myManage.addUser(strName, strSurname, strUser, strPassword);
+
+                }   // for
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }   // onPost
+
+    }   // Connected Class
+
+
+    private void synMySQLtoSQLite() {
+        ConnectedUserJSON connectedUserJSON = new ConnectedUserJSON();
+        connectedUserJSON.execute();
+    }
 
     private void deleteAllSQLite() {
 
